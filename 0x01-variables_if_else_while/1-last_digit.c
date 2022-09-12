8dcc4248f4e210af6000472b13d9ac65a94aa386
Ofili Oluchukwu <ofilioluchukwuchibundu@gmail.com>
2022-09-12 02:54:29 -0700

    The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#!/bin/bash
+#include &lt;stdlib.h&gt;
+#include &lt;time.h&gt;
+#include &lt;stdio.h&gt
+/**
+ * main -Prints random number to variable
+ *
+ * Return: Always (Successful)
+ */
+int main(void)
+{
+	int n;
+	char last[] = &quot;Last digit of &quot;;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf(&quot%s %d is %d and is &quot;, last, n, n % 10);
+	if (n % 10 &gt; 5)
+	{
+		printf(&quot;greater than 5\n&quot;);
+	}
+	else if (n % 10 == 0)
+	{
+		printf(&quot;0\n&quot;);
+	}
+	else
+	{
+		printf(&quot;less than 6 and not 0\n&quot;);
+	}
+	return (0);
+}
